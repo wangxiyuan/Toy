8038c784971dd47d9521d2a1de94ba360857fb11
wangxiyuan <wangxiyuan1007@gmail.com>
2020-08-03 16:23:41 +0800

    Update analysis.py

--- log_analysis/analysis.py
+++ log_analysis/analysis.py
@@ -2,6 +2,7 @@
 
 import csv
 from datetime import datetime
+import json
 import os
 import requests
 import subprocess
@@ -13,14 +14,16 @@ OUTPUT_FILE = "./result.csv"
 
 
 def monitorLog(logFile):
-    print("monitor logFile: %s" % LOG_FILE)
-    popen = subprocess.Popen(["cat", LOG_FILE], stdout=subprocess.PIPE,
+    print("monitor logFile: %s" % logFile)
+    popen = subprocess.Popen(["cat", logFile], stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE)
     while True:
         line = popen.stdout.readline().strip()
         if line:
             if '.whl' in line:
                 analysisLine(line)
+        else:
+            break
 
 
 def analysisLine(line):
@@ -30,10 +33,14 @@ def analysisLine(line):
 
 
 def getInfo(line):
+    # 208.91.105.88 - - [02/Aug/2020:16:16:57 +0000] "GET /logs/periodic-0/github.com/pytorch/pytorch/master/pytorch-arm64-build-daily-master-py36/540dac9/test_results/torch-1.7.0a0+d403983-cp36-cp36m-linux_aarch64.whl HTTP/1.1" 200 78250994 "https://status.openlabtesting.org/builds/build/540dac9c84634532b3dddcbe3769f1c2" "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36"
     # 208.91.105.88 - - [02/Aug/2020:16:18:20 +0000] "GET /favicon.ico HTTP/1.1" 404 560 "https://logs.openlabtesting.org/logs/periodic-0/github.com/pytorch/pytorch/master/pytorch-arm64-build-daily-master-py36/4e83d26/test_results/torch-1.7.0a0+c5b4f60-cp36-cp36m-linux_aarch64.whl" "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36"
     IP, res = line.split(' - - [')
     request_time, res = res.split('] ')
-    file_name = res.split("\" ")[1].split('/')[-1]
+    if 'whl' in res.split("\" ")[1]:
+        file_name = res.split("\" ")[1].split('/')[-1]
+    else:
+        file_name = res.split("\" ")[0].split('/')[-1][:-5]
 
     return IP, request_time, file_name
 
@@ -50,7 +57,7 @@ def writeToFile(request_ip, request_time, location, file_name):
     row = [request_ip, request_time, location, file_name]
 
     if not os.path.exists(OUTPUT_FILE):
-        with open(OUTPUT_FILE, 'x')as f:
+        with open(OUTPUT_FILE, 'w')as f:
             f_csv = csv.writer(f)
             f_csv.writerow(headers)
 
@@ -60,4 +67,4 @@ def writeToFile(request_ip, request_time, location, file_name):
 
 
 if __name__ == '__main__':
-    monitorLog(logFile)
+    monitorLog(LOG_FILE)
